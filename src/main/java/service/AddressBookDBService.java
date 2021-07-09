020b9fc5e5225993061115e4e0a2f96dd7db2c14
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-10 02:39:41 +0530

    added: updateContactDetails method to update data and sync with database

--- src/main/java/service/AddressBookDBService.java
+++ src/main/java/service/AddressBookDBService.java
@@ -2,6 +2,7 @@ package service;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,14 +14,16 @@ import exception.AddressBookJDBCException;
 
 public class AddressBookDBService {
 
+	List<ContactDetails> addressBookDataObj = null;
+
 	/**
 	 * created getConnection() method to make connection with mySQL database
 	 * 
 	 * @return connection
 	 * @throws AddressBookJDBCException
 	 */
-	private Connection getConnection() throws AddressBookJDBCException {
-		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+	public Connection getConnection() throws AddressBookJDBCException {
+		String jdbcURL = "jdbc:mysql://localhost:3306/addressbook_service?&useSSL=false";
 		String user = "root";
 		String password = "Database123@";
 		Connection connection;
@@ -54,13 +57,76 @@ public class AddressBookDBService {
 				String state = resultSet.getString("state");
 				int zip = resultSet.getInt("zip");
 				long phone_number = resultSet.getLong("phone_number");
+				String email_id = resultSet.getString("email_id");
 				String addressbook_name = resultSet.getString("addressbook_name");
 				String addressbook_type = resultSet.getString("addressbook_type");
 				addressBookList.add(new ContactDetails(first_name, last_name, address, city, state, zip, phone_number,
-						state, addressbook_name, addressbook_type));
+						email_id, addressbook_name, addressbook_type));
 			}
 		} catch (SQLException e) {
 			throw new AddressBookJDBCException("Unable to get data from address book");
+		}
+		return addressBookList;
+	}
+
+	/**
+	 * created updateAddressBookDataUsingPreparedStatement method to update data in database by using sql query
+     * added try and catch block to throw sql exception
+	 * @param state
+	 * @param zip
+	 * @param name
+	 * @return size of addressBookDataObj
+	 * @throws AddressBookJDBCException
+	 */
+	public int updateAddressBookDataUsingPreparedStatement(String state, int zip, String name)
+			throws AddressBookJDBCException {
+		String query = "update addressbook set state = ? , zip = ? where first_name = ?";
+		try (Connection connection = this.getConnection()) {
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, state);
+			preparedStatement.setInt(2, zip);
+			preparedStatement.setString(3, name);
+			int result = preparedStatement.executeUpdate();
+			addressBookDataObj = getAddressBookDataFromDB(name);
+			if (result > 0 && addressBookDataObj != null) {
+				((ContactDetails) addressBookDataObj).setState(state);
+				((ContactDetails) addressBookDataObj).setZip(zip);
+			}
+		} catch (SQLException e) {
+			throw new AddressBookJDBCException("Error in updation");
+		}
+		return addressBookDataObj.size();
+	}
+
+	/**
+	 * This method is used to retrieve data from database and check whether the values are updated or not 
+	 * if the name is matched with first name
+	 * @param name 
+	 * @return addressBookList having data according to the addressbok table of addressbook_service
+	 * @throws AddressBookJDBCException
+	 */
+	public List<ContactDetails> getAddressBookDataFromDB(String name) throws AddressBookJDBCException {
+		String query = "select * from addressbook where first_name=?;";
+		List<ContactDetails> addressBookList = new ArrayList<>();
+		try (Connection connection = this.getConnection()) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				String first_name = resultSet.getString("first_name");
+				String last_name = resultSet.getString("last_name");
+				String address = resultSet.getString("address");
+				String city = resultSet.getString("city");
+				String state = resultSet.getString("state");
+				int zip = resultSet.getInt("zip");
+				long phone_number = resultSet.getLong("phone_number");
+				String email_id = resultSet.getString("email_id");
+				String addressbook_name = resultSet.getString("addressbook_name");
+				String addressbook_type = resultSet.getString("addressbook_type");
+				addressBookList.add(new ContactDetails(first_name, last_name, address, city, state, zip, phone_number,
+						email_id, addressbook_name, addressbook_type));
+			}
+		} catch (SQLException e) {
+			throw new AddressBookJDBCException("Unable to get data.Please check table for updation");
 		}
 		return addressBookList;
 	}
--- src/main/java/service/AddressBookService.java
+++ src/main/java/service/AddressBookService.java
@@ -1,5 +1,65 @@
 package service;
 
-public class AddressBookService {
+import java.util.List;
 
+import exception.AddressBookJDBCException;
+import model.*;
+
+public class AddressBookService {
+	public AddressBookDBService addressBookDBService;
+	private List<ContactDetails> contactList;
+
+	public AddressBookService() {
+		this.addressBookDBService = new AddressBookDBService();
+	}
+
+	/**
+	 * created readAddressBookData method to read data from database
+	 * 
+	 * @return this.addressBookDBService.readData()
+	 * @throws AddressBookJDBCException
+	 */
+	public List<ContactDetails> readAddressBookData() throws AddressBookJDBCException {
+		return this.addressBookDBService.readData();
+	}
+
+	/**
+	 * This method update the contact if the first_name matches in the addressbook table
+	 * @param name
+	 * @param state
+	 * @param zip
+	 * @throws AddressBookJDBCException
+	 */
+	public void updateContactDetails(String state, int zip, String name) throws AddressBookJDBCException {
+		int result = new AddressBookDBService().updateAddressBookDataUsingPreparedStatement(state, zip, name);
+		if (result == 0)
+			return;
+		ContactDetails addressBookData = this.getContactDetails(name);
+		if (addressBookData != null) {
+			addressBookData.setZip(zip);
+			addressBookData.setState(state);
+		}
+	}
+
+	/**
+	 * getContactDetails method to get the data of person from contact details list y using stream api
+	 * @param name : first_name
+	 * @return first_name if it matches with the name present in the database
+	 */
+	private ContactDetails getContactDetails(String name) {
+		return this.contactList.stream()
+				.filter(addressBookDataListObject -> addressBookDataListObject.getFirst_name().equals(name)).findFirst()
+				.orElse(null);
+	}
+
+	/**
+	 * checkAddressBookInSyncWithDB method check update details whether synced or not with database
+	 * @param name : first name of contact details
+	 * @return synced contact list
+	 * @throws AddressBookJDBCException
+	 */
+	public boolean checkAddressBookInSyncWithDB(String name) throws AddressBookJDBCException {
+		List<ContactDetails> contactList = new AddressBookDBService().getAddressBookDataFromDB(name);
+		return contactList.get(0).equals(getContactDetails(name));
+	}
 }
